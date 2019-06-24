b33d2df1952a409a248d35ea70ce2a92166df7e6
Antonio <bandafred@mail.ru>
2019-06-24 21:33:23 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Тестовое задание</span>
+        <span class="font-weight-light"> файловый менеджер</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <FileManager/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import FileManager from './components/FileManager'
+
+export default {
+  name: 'App',
+  components: {
+    FileManager
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
